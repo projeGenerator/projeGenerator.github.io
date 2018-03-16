d01848e91d8af51c8a8053c32bcb949b47a12ca5
Gabriel Silveira <gabomfim99@gmail.com>
2018-03-16 04:53:00 -0300

    changed the title and made the message fixed

--- js/controllers/projeCtrl.js
+++ js/controllers/projeCtrl.js
@@ -1,6 +1,4 @@
 angular.module("proje").controller("projeCtrl", function($scope){
-	$scope.app = "P R O G\xa0\xa0G E N E R A T O R";
-	$scope.mensagem = "clique no botão para gerar um prog";
 	$scope.urls = [['-Tdu4uKSZ3M', 'LdpMpfp-J_I', 'Gcagz8epRN8','R2F_hGwD26g','1VigPPJ6j40'],[0,0,0,0,0],['Yes','Rush','Camel','Tool','Dream Theater'],['Roundabout','YYZ','Freefall','The Pot','Learning to Live'],['/yes/43560/','instrumental','/camel/161317/','/tool/535467/','/dream-theater/11769/']];//youtube link, random repetition controller, band name, song name, letras.mus link for the lyrics scavenger
 	$scope.noMoreLinks = true;
 	$scope.youtube = false
